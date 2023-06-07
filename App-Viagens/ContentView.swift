ab69e15dcadca8a3151dfca1291106dee9cd78a4
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-06-07 15:50:17 -0300

    Tela de catalogo de viagens inicializado

--- App-Viagens/ContentView.swift
+++ App-Viagens/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Mello Viagens")
+                .foregroundColor(.white)
+                .font(.custom("Avenir Black", size: 25))
+                
+            
+            Text("Especial".uppercased())
+                .foregroundColor(.white)
+                .font(.custom("Avenir Book", size: 18))
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 30)
+            
+            Text("Mundo".uppercased())
+                .font(.custom("Avenir Black", size: 20))
+                .foregroundColor(.white)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 30)
+                
+            List {
+                Text("Miami")
+                Text("Paris")
+                Text("Veneza")
+                Text("Berlin")
+                Text("Amsterdam")
+                Text("Nova York")
+            }
         }
-        .padding()
+        .background(Color.teal)
+
     }
 }
 

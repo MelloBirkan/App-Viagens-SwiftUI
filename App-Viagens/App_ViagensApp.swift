56725431ef9335c9244ec8f3a056286137e85ede
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-06-07 14:45:14 -0300

    Initial Commit

---
+++ App-Viagens/App_ViagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ViagensApp.swift
+//  App-Viagens
+//
+//  Created by Marcello Gonzatto Birkan on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct App_ViagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

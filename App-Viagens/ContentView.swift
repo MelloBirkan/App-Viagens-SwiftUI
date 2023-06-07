d907437e327b75b97a69dfbaba8ee18cbab8610f
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-06-07 16:14:54 -0300

    GeometryReader Usado para ajustar Header

--- App-Viagens/ContentView.swift
+++ App-Viagens/ContentView.swift
@@ -9,35 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Mello Viagens")
-                .foregroundColor(.white)
-                .font(.custom("Avenir Black", size: 25))
-                
+        
+        GeometryReader { View in
             
-            Text("Especial".uppercased())
-                .foregroundColor(.white)
-                .font(.custom("Avenir Book", size: 18))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
+            // MARK: - VStack Principal
             
-            Text("Mundo".uppercased())
-                .font(.custom("Avenir Black", size: 20))
-                .foregroundColor(.white)
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
+            VStack {
                 
-            List {
-                Text("Miami")
-                Text("Paris")
-                Text("Veneza")
-                Text("Berlin")
-                Text("Amsterdam")
-                Text("Nova York")
+                // MARK: - VStack Header
+                
+                VStack{
+                    Text("Mello Viagens")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 25))
+                        .padding(.top, 30)
+                    
+                    
+                    Text("Especial".uppercased())
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Book", size: 18))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    
+                    Text("Mundo".uppercased())
+                        .font(.custom("Avenir Black", size: 20))
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: View.size.width, height: 180, alignment: .top)
+                .background(Color.teal)
+                
+                    List {
+                        Text("Miami")
+                        Text("Paris")
+                        Text("Veneza")
+                        Text("Berlin")
+                        Text("Amsterdam")
+                        Text("Nova York")
+                    }
             }
+            
         }
-        .background(Color.teal)
-
     }
 }
 
